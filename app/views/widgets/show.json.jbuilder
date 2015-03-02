3dc00510e235c20e5a6ce6ed350216855c46ba85
Joseph Siefers <joseph.siefers@gmail.com>
2015-03-02 18:29:45 -0500

    Initial Commit.

---
+++ app/views/widgets/show.json.jbuilder
@@ -0,0 +1,1 @@
+json.extract! @widget, :id, :text, :created_at, :updated_at
---
+++ db/migrate/20150302194158_create_widgets.rb
@@ -0,0 +1,9 @@
+class CreateWidgets < ActiveRecord::Migration
+  def change
+    create_table :widgets do |t|
+      t.string :text
+
+      t.timestamps null: false
+    end
+  end
+end
